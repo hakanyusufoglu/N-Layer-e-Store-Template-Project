988faa13fc9c221e2527fd11b9476cd3ca06013a
Hakan Yusufoğlu <hakan_00133@hotmail.com>
2021-01-13 18:33:45 +0300

    Data layer editted. Service layer insterted

---
+++ NLayereStoreTemplateProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using NLayereStoreTemplateProject.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayereStoreTemplateProject.Data.UnitOfWorks
+{
+    //tüm işlemler onaylandıktan sonra tek bir adım ile veritabanına veri kaydetme, değiştirme işlemleri gerçekleştirilmektedir.
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        public UnitOfWork(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+           await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ NLayereStoreTemplateProject.Service/Services/Service.cs
@@ -0,0 +1,67 @@
+﻿using NLayereStoreTemplateProject.Core.Repositories;
+using NLayereStoreTemplateProject.Core.Services;
+using NLayereStoreTemplateProject.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayereStoreTemplateProject.Service.Services
+{
+    class Service<TEntity> : IService<TEntity> where TEntity : class
+    {
+        public readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<TEntity> _repository;
+        public Service(IUnitOfWork unitOfWork,IRepository<TEntity> repository)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = repository;
+        }
+        public async Task AddAsync(TEntity entity)
+        {
+            await _repository.AddAsync(entity);
+            await _unitOfWork.CommitAsync();
+        }
+
+        public Task AddRangeAsync(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
