3d3d3d9d1377e355de92eca5343930a0a147f71e
Hakan Yusufoğlu <hakan_00133@hotmail.com>
2021-01-13 15:26:23 +0300

    Basic Repositories, Services, UnitOfWork interfaces added (in core layer)

---
+++ NLayereStoreTemplateProject.Core/Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayereStoreTemplateProject.Core.Repositories
+{
+    //Tüm entity için ortak olan metotların tanımlanması;
+    public interface IRepository<TEntity> where TEntity:class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(int id); //ID'ye göre generic tipte ki entity hangisiyse onu getir.
+        Task<IEnumerable<TEntity>> Where(Expression<Func<TEntity, bool>> predicate); //TEntity olan ve tipide bool olan bir metodu işaret et... 
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        TEntity Update(TEntity entity);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
---
+++ NLayereStoreTemplateProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayereStoreTemplateProject.Core.UnitOfWorks
+{
+   public interface IUnitOfWork
+    {
+        Task CommitAsync(); //eğer database'e savechangeAsync() metodu istenirse bu metotla çağırılacak.
+        void Commit(); //eğer database'e savechange() metodu istenirse bu metotla çağırılacak.
+    }
+}
