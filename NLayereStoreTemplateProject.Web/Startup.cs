c02eec74945ea9670d34196ccefa0d7438c9baae
Hakan Yusufoğlu <hakan_00133@hotmail.com>
2021-01-26 16:17:54 +0300

    Basic Web Template Created -Boostrap4.5 Files Added -index.html Created

--- NLayereStoreTemplateProject.Web/Startup.cs
+++ NLayereStoreTemplateProject.Web/Startup.cs
@@ -50,7 +50,7 @@ namespace NLayereStoreTemplateProject.Web
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home2}/{action=Index}/{id?}");
             });
         }
     }
