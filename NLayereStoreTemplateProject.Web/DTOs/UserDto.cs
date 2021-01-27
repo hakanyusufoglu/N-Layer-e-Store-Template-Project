0275bd50e18f26a576bd2174a02e119486363778
Hakan Yusufoğlu <hakan_00133@hotmail.com>
2021-01-27 17:08:38 +0300

    yorum satiri deneme

--- NLayereStoreTemplateProject.Web/DTOs/UserDto.cs
+++ NLayereStoreTemplateProject.Web/DTOs/UserDto.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace NLayereStoreTemplateProject.Web.DTOs
 {
     public class UserDto
-    {
+    {//yorum satırı deneme amaçlı eklendi
         public int UserId { get; set; }
         [Required]
         public string UserName { get; set; }
