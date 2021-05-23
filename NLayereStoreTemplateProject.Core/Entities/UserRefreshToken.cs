1a6e2eb96217c9bbecbe0d025a043bd6976a95b2
Hakan Yusufoğlu <hakan_00133@hotmail.com>
2021-05-23 20:16:09 +0300

    Migration project 3.1 to 5.0 and userapp and roleapp classes created in core layer for token authorization (with JWT)

---
+++ NLayereStoreTemplateProject.Core/Entities/UserRefreshToken.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLayereStoreTemplateProject.Core.Entities
+{
+    public class UserRefreshToken
+    {
+
+    }
+}
